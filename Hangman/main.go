a72b91cffb298fa8bd68f9837cf97311d31c1713
Girgis Emad <girgisemad74@gmail.com>
2024-10-20 16:07:46 +0300

    Add hangman Game

---
+++ Hangman/main.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"strings"
+	"time"
+	"unicode/utf8"
+)
+
+func main() {
+
+
+	// pick random word
+	source := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(source)
+	sliceOfword := []string{"golang", "javaScript", "python", "java", "haskell", "rust", "zig"}
+	word := sliceOfword[random.Intn(len(sliceOfword))]
+
+	// max false trys is 6 create '_''_''_' as size of the random word
+	// create scanner also to take input for the user
+	attempts := 6
+	currentWordState := initWordState(word)
+	scanner := bufio.NewScanner(os.Stdin)
+	gussedLetters := make(map[string]bool)
+
+	// start the game 
+	fmt.Println("Welcome to hangman game...")
+	time.Sleep(4 * time.Second)
+
+	for attempts > 0 {
+		clearConsole()
+		// print the ascii code draw 
+		fmt.Println(AsciiStatesDrawHangman[attempts])
+		displayCurrentState(currentWordState)
+		userInput := getUserInput(scanner)
+
+
+		if !isValidInput(userInput) {
+			fmt.Println("Invalid input  please inter single character")
+			time.Sleep(3 * time.Second)
+			continue
+		} else if gussedLetters[userInput] {
+			fmt.Println("You already guessed that letter .")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		isCorrectGuess := updateWordIfCorrect(word, currentWordState, userInput)
+		isTheWordCompleted := isCompleteWord(currentWordState)
+		gussedLetters[userInput] = true
+
+		if isCorrectGuess && isTheWordCompleted {
+			clearConsole()
+			println("You are the  winner the word is ", word)
+			break
+		} else if !isCorrectGuess {
+			attempts--
+		}
+		if attempts < 0 {
+			clearConsole()
+			fmt.Println("The man Is dead")
+			break
+		}
+
+	}
+
+}
+
+func getUserInput(scanner *bufio.Scanner) string {
+	scanner.Scan()
+	return scanner.Text()
+}
+
+func initWordState(word string) []string {
+	currentWordState := make([]string, len(word))
+
+	for i := range currentWordState {
+		currentWordState[i] = "_"
+	}
+	return currentWordState
+}
+
+func isValidInput(input string) bool {
+  if _,err := strconv.Atoi(input) ; err==nil || utf8.RuneCountInString(input)>1 || input=="" {
+     return false      
+  }
+  return true
+}
+
+func displayCurrentState(currentWord []string) {
+	fmt.Println("Current word state : ", strings.Join(currentWord, ""))
+
+}
+
+func updateWordIfCorrect(word string, currentWordState []string, letter string) bool {
+	updateWordIfCorrect := false
+
+	for i, char := range word {
+		if string(char) == letter {
+			currentWordState[i] = letter
+			updateWordIfCorrect = true
+
+		}
+	}
+	return updateWordIfCorrect
+}
+
+func isCompleteWord(word []string) bool {
+	for _, char := range word {
+		if char == "_" {
+			return false
+		}
+	}
+	return true
+}
+
+func clearConsole() {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd", "/c", "cls")
+	} else {
+		cmd = exec.Command("clear")
+	}
+	cmd.Stdout = os.Stdout
+	cmd.Run()
+}
